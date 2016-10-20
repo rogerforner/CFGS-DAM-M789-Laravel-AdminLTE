6ffeea459b6335323621e6829935402d9e7413e5
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-20 21:22:31 +0200

    Estils CSS i LESS

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,11 @@ require('laravel-elixir-vue-2');
 
 elixir(mix => {
     mix.sass('app.scss')
-       .less('app.less')
+       .less('app-less.less')
+       .less('./node_modules/admin-lte/build/less/AdminLTE.less','./public/css/adminlte-less.css')
+       .styles([
+           './public/css/app.css',
+           './public/css/adminlte-less.css'
+       ])
        .webpack('app.js');
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Adminlte Tinkering</title>
 
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/all.css">
 </head>
 <body>
 Hola!
